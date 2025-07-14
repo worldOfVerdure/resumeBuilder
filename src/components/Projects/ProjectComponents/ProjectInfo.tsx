f5ecf808ded644f59bea16fed658e817bf714533
worldOfVerdure <perspicaciousdotard@gmail.com>
2025-07-14 10:51:00 -0500

    Fix ProjectInfo to be type annotated. Begin to use Record type utility

--- src/components/Projects/ProjectComponents/ProjectInfo.tsx
+++ src/components/Projects/ProjectComponents/ProjectInfo.tsx
@@ -1,43 +1,25 @@
 import DESCRIPTION from "./helpers/descriptions/generic.ts"; //Change file name for different text.
 import { type FC } from "react";
 import { indexRetrieval } from "./helpers/projectHelperFunctions.ts";
-import { type ProjectType } from "./helpers/projectData.ts";
 import { styled } from "styled-components";
 
-const ProjectInfo: FC<ProjectType> = ({ title, tech, githubLink }) => {
-  const projectIndex: number = indexRetrieval(title);
-  console.log(projectIndex);
-  const techString = tech?.join(", ");
+type ProjectInfoProps = {
+  projectTitle: string;
+};
+
+const ProjectInfo: FC<ProjectInfoProps> = ({ projectTitle }) => {
+   const projectIndex: number = indexRetrieval(projectTitle);
+  
   return (
-    <>
-      <ProjectInforHeader>
-        <h3>{title}</h3>
-        |
-        <p>{techString}</p>
-        |
-        <a href={githubLink} target="_blank" >GitHub</a>
-      </ProjectInforHeader>
-      <ProjectListContainer>
-        {DESCRIPTION[projectIndex].map((listPoint, index) => {
-          return <li key={`${title} li ${index}`} >{listPoint}</li>;
-        })}
-      </ProjectListContainer>
-    </>
+    <ProjectListContainer>
+      {DESCRIPTION[projectIndex].map((listPoint, index) => {
+        return <li key={`${projectTitle} li ${index}`} >{listPoint}</li>;
+      })}
+    </ProjectListContainer>
   );
 }
 
 export default ProjectInfo;
-
-const ProjectInforHeader = styled.div`
-  align-items: center;
-  display: flex;
-  gap: 1rem;
-  margin-bottom: .4rem;
-
-  & h3, p {
-    margin: 0;
-  }
-`;
 
 const ProjectListContainer = styled.ul`
   & li {
--- src/components/Projects/ProjectComponents/SingularProject.tsx
+++ src/components/Projects/ProjectComponents/SingularProject.tsx
@@ -1,12 +1,21 @@
 import { type FC } from "react";
+
 import ProjectInfo from "./ProjectInfo.tsx";
 import { type ProjectType } from "./helpers/projectData.ts";
 import { styled } from "styled-components";
 
 const SingularProject: FC<ProjectType> = (props) => {
+  const techString = props.tech?.join(", ");
   return (
     <ProjectContainer>
-      <ProjectInfo {...props} />
+      <ProjectInforHeader>
+        <h3>{props.title}</h3>
+        |
+        <p>{techString}</p>
+        |
+        <a href={props.githubLink} target="_blank" >GitHub</a>
+      </ProjectInforHeader>
+      <ProjectInfo projectTitle={props.title} />
     </ProjectContainer>
   );
 }
@@ -18,6 +27,17 @@ const ProjectContainer = styled.ul`
   padding: 0;
 
   &:last-child {
+    margin: 0;
+  }
+`;
+
+const ProjectInforHeader = styled.div`
+  align-items: center;
+  display: flex;
+  gap: 1rem;
+  margin-bottom: .4rem;
+
+  & h3, p {
     margin: 0;
   }
 `;
